7826423f690b95921bc833cbebcfbcd83fd39064
Paul Barry <mail@paulbarry.com>
2015-02-16 19:25:32 -0500

    Require username for message, show error if not present

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,13 +1,18 @@
 class MessagesController < ApplicationController
   def show
     @messages = Message.order('created_at desc').all
+    @message = Message.new
   end
 
   def create
-    Message.create(
+    @message = Message.new(
       username: params[:username],
       message: params[:message]
     )
-    redirect_to '/messages'
+    if @message.save
+      redirect_to '/messages'
+    else
+      render 'show'
+    end
   end
 end
